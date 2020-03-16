c7c5585a09dea9b0da4c91a425173e19805bc2df
Hartono Chandra <hartono.chandra@traveloka.com>
2020-03-16 19:23:18 +0700

    remake node version

--- buffertable-node/src/bbuffer.ts
+++ buffertable-node/src/bbuffer.ts
@@ -3,112 +3,89 @@ import { BType, STR_SIZE_TYPE, BSIZE } from './constants';
 import { staticImplements } from './utils';
 
 const BFuncKey = [
-  'Uint8',
-  'Uint16',
-  'Uint32',
+  'UInt8',
+  'UInt16BE',
+  'UInt32BE',
   'Int8',
-  'Int16',
-  'Int32',
-  'Float32',
-  'Float64',
-  'Uint8'
+  'Int16BE',
+  'Int32BE',
+  'FloatBE',
+  'DoubleBE',
+  'UInt8'
 ];
-
-const encoder = new TextEncoder();
-const decoder = new TextDecoder();
 
 @staticImplements<BBufferStaticInterface>()
 export class BBuffer implements BBufferInterface {
-  private arr: Uint8Array;
-  private dv: DataView;
+  private buffer: Buffer;
 
   private constructor() {
     // noop
   }
 
   slice(start = 0, end?: number): BBuffer {
-    return BBuffer.from(this.arr.slice(start, end));
+    return BBuffer.from(this.buffer.slice(start, end));
   }
 
   set(bBuffer: BBuffer, offset?: number) {
-    this.arr.set(bBuffer.arr, offset);
+    this.buffer.set(bBuffer.buffer, offset);
   }
 
   fill(buffer: Uint8Array, offset?: number) {
-    this.arr.set(buffer, offset);
+    this.buffer.set(buffer, offset);
   }
 
   write(type: BType, offset: number, value: BValue) {
     if (type >= BType.U8 && type <= BType.F64) {
-      this.dv[`set${BFuncKey[type]}`](offset, value as number);
+      this.buffer[`write${BFuncKey[type]}`](value as number, offset);
     } else if (type === BType.BOOL) {
-      this.dv[`set${BFuncKey[type]}`](offset, value ? 1 : 0);
+      this.buffer[`write${BFuncKey[type]}`](value ? 1 : 0, offset);
     }
   }
 
   read(type: BType, offset: number): BValue {
     if (type >= BType.U8 && type <= BType.F64) {
-      return this.dv[`get${BFuncKey[type]}`](offset);
+      return this.buffer[`read${BFuncKey[type]}`](offset);
     } else if (type === BType.BOOL) {
-      return this.dv[`get${BFuncKey[type]}`](offset) > 0;
+      return this.buffer[`read${BFuncKey[type]}`](offset) > 0;
     } else if (type === BType.STR) {
       const size = this.read(STR_SIZE_TYPE, offset) as number;
       const dataOffset = offset + BSIZE[STR_SIZE_TYPE];
 
-      return decoder.decode(this.arr.slice(dataOffset, dataOffset + size));
+      return this.buffer.toString('utf-8', dataOffset, dataOffset + size);
     }
   }
 
   toUint8Array(): Uint8Array {
-    return this.arr;
+    return this.buffer;
   }
 
   static create(size: number): BBuffer {
     const result = new BBuffer();
-
-    const ab = new ArrayBuffer(size);
-    result.arr = new Uint8Array(ab);
-    result.dv = new DataView(ab);
+    result.buffer = Buffer.allocUnsafe(size);
 
     return result;
   }
 
   static from(buffer: Uint8Array): BBuffer {
     const result = new BBuffer();
-
-    result.arr = buffer;
-    result.dv = new DataView(buffer.buffer);
+    result.buffer = Buffer.from(buffer);
 
     return result;
   }
 
   static fromStr(value: string): BBuffer {
-    const encodedBuffer = encoder.encode(value);
-    const valueSize = encodedBuffer.byteLength;
+    const valueSize = Buffer.byteLength(value);
     const strTypeSize = BSIZE[STR_SIZE_TYPE];
 
     const result = BBuffer.create(valueSize + strTypeSize);
 
     result.write(STR_SIZE_TYPE, 0, valueSize);
-    result.fill(encodedBuffer, strTypeSize);
+    result.buffer.fill(value, strTypeSize, strTypeSize + valueSize, 'utf-8');
 
     return result;
   }
 
   static concat(buffers: Uint8Array[]): Uint8Array {
-    const size = buffers.reduce(
-      (prev, current) => prev + current.byteLength,
-      0
-    );
-
-    const result = new Uint8Array(size);
-    let offset = 0;
-
-    buffers.forEach(b => {
-      result.set(b, offset);
-      offset += b.byteLength;
-    });
-
-    return result;
+    return Buffer.concat(buffers);
   }
 }
